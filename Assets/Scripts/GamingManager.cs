eb961eb670f6e1997cae48d839d25fc853cbc771
B. Bueno <afterglow9000@gmail.com>
2020-01-26 05:42:35 -0700

    initial commit

---
+++ Assets/Scripts/GamingManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamingManager : MonoBehaviour {
+
+    public float delay = 1f;
+    private bool hasEnded = false;
+    public GameObject levelCompleteUI;
+
+    public void EndGame () {
+
+        if (hasEnded == false) {
+            hasEnded = true;
+            Invoke("Restart", delay);
+        }
+
+    }
+
+    public void LevelComplete () {
+
+        levelCompleteUI.SetActive(true);
+
+    }
+
+    private void Restart () {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float forwardForce = 2000f;
+    public Rigidbody rB;
+    public float sideForce = 500f;
+
+    private void FixedUpdate () {
+
+        rB.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+            rB.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        if (Input.GetKey("a"))
+            rB.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        if (rB.position.y < -1f)
+            FindObjectOfType<GamingManager>().EndGame();
+
+	}
+
+}
